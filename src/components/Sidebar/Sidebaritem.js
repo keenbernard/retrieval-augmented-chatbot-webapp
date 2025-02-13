934d64b77f8b8eacd2c81c2bf66c710a747b542b
k.bernard <k.bernard@belizetelemedia.net>
2025-02-13 14:01:34 -0600

    Created features and components to support MSAL integration for SSL. Downgraded dependencies to support change.

--- src/components/Sidebar/Sidebaritem.js
+++ src/components/Sidebar/Sidebaritem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './SidebarItem.css';
 
 const SidebarItem = ({ title, isActive, onClick }) => {
-    return (
-        <div
-            className={`sidebar-item ${isActive ? 'active' : ''}`} // Add 'active' class if isActive is true
-            onClick={onClick}
-        >
-            {title}
-        </div>
-    );
+  return (
+      <div
+          className={`sidebar-item ${isActive ? 'active' : ''}`} // Add 'active' class if isActive is true
+          onClick={onClick}
+      >
+        {title}
+      </div>
+  );
 };
 
 export default SidebarItem;
